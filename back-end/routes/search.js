61c58812e72d759a2a5f1ebe7b99f13f46c04099
jjtu WSL2 box <gmail>
2021-04-21 17:52:47 -0400

    orWhere implemented to search.js, still working

--- back-end/routes/search.js
+++ back-end/routes/search.js
@@ -5,49 +5,34 @@ const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV])
 
 router.get('/', function(req, res, next) {
     console.log(req.query); //http://localhost:3001/search?make=Ford&model=Explorer
-    switch (true) {
-      //If model is defined.
-      case (req.query.model === undefined):
-        knex
-          .select('*')
-          .from('vehicles')
-          .where('make', req.query.make)
-          .then(data => res.status(200).json(data))
-          .catch(err =>
-            res.status(404).json({
-              message:
-                'The data you are looking for could not be found. Please try again'
-            })
-          );
-        break;
-
-      case (req.query.color === undefined):
-        knex
-          .select('*')
-          .from('vehicles')
-          .where('make', req.query.make)
-          .where('model', req.query.model)
-          .then(data => res.status(200).json(data))
-          .catch(err =>
-            res.status(404).json({
-              message:
-                'The data you are looking for could not be found. Please try again'
-            })
-          );
-        break;
-      
-      default:
-        knex
-          .select('*')
-          .from('vehicles')
-          .then(data => res.status(200).json(data))
-          .catch(err =>
-            res.status(404).json({
-              message:
-                'The data you are looking for could not be found. Please try again'
-            })
-          );
-    }
+      knex
+        .select('*')
+        .from('vehicles')
+        //Check for VIN
+        .where('VIN', req.query.vin)
+        //Check for type
+        .orWhere('type', req.query.type)
+        //Check for year
+        .orWhere('year', req.query.year)
+        //Check for make
+        .orWhere('make', req.query.make)
+        //Check for model
+        .orWhere('model', req.query.model)
+        //Check for color
+        .orWhere('color', req.query.color)
+        //Check for mileage
+        .orWhere('mileage', req.query.mileage)
+        //Check for price
+        .orWhere('price', req.query.price)
+        //Check for isNew
+        .orWhere('isNew', req.query.isNew)
+        .then(data => res.status(200).json(data))
+        .catch(err =>
+          res.status(404).json({
+            message:
+              'The data you are looking for could not be found. Please try again'
+          })
+        );
 });
 
 

e221b845a7e64dcfffd27ce15d41b75160aeba07
Amin Yachnes <Amin.Yachnes.1@us.af.mil>
2021-04-22 11:42:21 -0400

    search filtering completed

--- back-end/routes/search.js
+++ back-end/routes/search.js
@@ -5,28 +5,22 @@ const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV])
 
 router.get('/', function(req, res, next) {
     console.log(req.query); //http://localhost:3001/search?make=Ford&model=Explorer
-      knex
-        .select('*')
-        .from('vehicles')
-        //Check for VIN
-        .where('VIN', req.query.vin)
-        //Check for type
-        .orWhere('type', req.query.type)
-        //Check for year
-        .orWhere('year', req.query.year)
-        //Check for make
-        .orWhere('make', req.query.make)
-        //Check for model
-        .orWhere('model', req.query.model)
-        //Check for color
-        .orWhere('color', req.query.color)
-        //Check for mileage
-        .orWhere('mileage', req.query.mileage)
-        //Check for price
-        .orWhere('price', req.query.price)
-        //Check for isNew
-        .orWhere('isNew', req.query.isNew)
-        .then(data => res.status(200).json(data))
+
+
+    var query = knex.select('*').from('vehicles');
+
+      //wall of if statements, checks if the param exists, if it does then it adds to the query results
+      if(req.query.vin !== undefined) {query.where('VIN', req.query.vin)}
+      if(req.query.type !== undefined) {query.where('type', req.query.type)}
+      if(req.query.year !== undefined) {query.where('year', req.query.year)}
+      if(req.query.make !== undefined) {query.where('make', req.query.make)}
+      if(req.query.model !== undefined) {query.where('model', req.query.model)}
+      if(req.query.color !== undefined) {query.where('color', req.query.color)}
+      if(req.query.mileage !== undefined) {query.where('mileage', req.query.mileage)}
+      if(req.query.price !== undefined) {query.where('price', req.query.price)}
+      if(req.query.isNew !== undefined) {query.where('isNew', req.query.isNew)}
+      
+      query.then(data => res.status(200).json(data))
         .catch(err =>
           res.status(404).json({
             message:
